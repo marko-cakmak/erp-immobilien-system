66e42c6620b2f9c06c0f0e1455cb183c0b8a292d
marko-cakmak <markocakmak.work@gmail.com>
2026-04-08 13:31:02 +0200

    Refactor seeder for dynamic apartment images

--- database/seeders/ApartmentImageSeeder.php
+++ database/seeders/ApartmentImageSeeder.php
@@ -10,24 +10,50 @@ class ApartmentImageSeeder extends Seeder
 {
     public function run(): void
     {
-        $apartment = Apartment::first();
+        $apartments = Apartment::all();
 
-        if (! $apartment) {
-            return;
+        foreach ($apartments as $apartment) {
+
+            $sourceFolder = database_path("seeders/images/apartments/{$apartment->id}");
+
+            if (!is_dir($sourceFolder)) {
+                continue;
+            }
+
+            $destinationFolder = storage_path("app/public/apartments/{$apartment->id}");
+
+            if (file_exists($destinationFolder)) {
+                array_map('unlink', glob($destinationFolder . '/*'));
+            } else {
+                mkdir($destinationFolder, 0777, true);
+            }
+
+            ApartmentImage::where('apartment_id', $apartment->id)->delete();
+
+            $files = array_values(array_diff(scandir($sourceFolder), ['.', '..']));
+            sort($files);
+
+            $position = 1;
+
+            foreach ($files as $file) {
+
+                $sourcePath = $sourceFolder . '/' . $file;
+
+                if (!is_file($sourcePath)) {
+                    continue;
+                }
+
+                copy($sourcePath, $destinationFolder . '/' . $file);
+
+                ApartmentImage::create([
+                    'apartment_id' => $apartment->id,
+                    'path' => "apartments/{$apartment->id}/{$file}",
+                    'position' => $position,
+                    'is_cover' => $position === 1,
+                ]);
+
+                $position++;
+            }
         }
-
-        ApartmentImage::create([
-            'apartment_id' => $apartment->id,
-            'path' => 'apartments/' . $apartment->id . '/living-room.jpg',
-            'position' => 1,
-            'is_cover' => true,
-        ]);
-
-        ApartmentImage::create([
-            'apartment_id' => $apartment->id,
-            'path' => 'apartments/' . $apartment->id . '/bedroom.jpg',
-            'position' => 2,
-            'is_cover' => false,
-        ]);
     }
 }
